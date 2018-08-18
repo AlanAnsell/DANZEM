8e74ddd0f8c2ad0d6c02750d3a1168a619d95b37
Alan Ansell <user.name>
2018-08-18 17:25:28 +1200

    make some improvements to model

--- danzem/model.py
+++ danzem/model.py
@@ -53,6 +53,7 @@ def KerasTrainFn(model_fn, eval_fn,
                  max_val_stagnation_epochs=10,
                  progress_check_epoch=5,
                  progress_check_thresh=0.3,
+                 batch_size=None,
                  verbose=False,
                  **kwargs):
     model_ = model_fn(train_X)
@@ -76,7 +77,7 @@ def KerasTrainFn(model_fn, eval_fn,
     val_stagnation_epochs = 0
     best_val_loss = val_loss
     while True:
-        model_.fit(train_X, train_y, epochs=1, verbose=0)
+        model_.fit(train_X, train_y, batch_size=batch_size, epochs=1, verbose=0)
         train_loss = eval_fn(model_, train_X, train_y, **kwargs).loss
         val_loss = eval_fn(model_, val_X, val_y, **kwargs).loss
         if verbose:
@@ -103,6 +104,9 @@ def KerasTrainFn(model_fn, eval_fn,
     best_model = models.load_model(model_file_path)
     os.remove(model_file_path)
     os.rmdir(model_file_dir)
+
+    if 'max_acceptable_loss' in kwargs and best_val_loss > kwargs['max_acceptable_loss']:
+        return None, 0.0
 
     return best_model, best_val_loss
 
@@ -138,6 +142,7 @@ def DefaultTrainFn(model_fn, eval_fn, X, y,
                    process_fn=None,
                    separate_transform_for_val=False,
                    n_models=1,
+                   max_attempts=-1,
                    verbose=False,
                    **kwargs):
 
@@ -157,7 +162,9 @@ def DefaultTrainFn(model_fn, eval_fn, X, y,
     best_model = None
     best_loss = 1e10
     n_successful_models = 0
-    while n_successful_models < n_models:
+    n_attempts = 0
+    while ((n_successful_models < n_models) and
+           (max_attempts == -1 or n_attempts < max_attempts)):
         model_, val_loss = single_model_train_fn(
                 model_fn, eval_fn, train_X, train_y, val_X, val_y,
                 verbose=verbose,
@@ -167,6 +174,7 @@ def DefaultTrainFn(model_fn, eval_fn, X, y,
                 best_loss = val_loss
                 best_model = model_
             n_successful_models += 1
+        n_attempts += 1
 
     if verbose:
         print('Best loss: %.5f' % best_loss)
@@ -187,8 +195,22 @@ _LOSS_FN_MAP = {'mae': metrics.mean_absolute_error,
                 'mse': metrics.mean_squared_error,
                 'accuracy': metrics.accuracy_score}
 
-_PROBA_LOSS_FN_MAP = {'log_loss': lambda y_true, y_pred:
-                      metrics.log_loss(y_true, y_pred, eps=1e-6)}
+
+def PredictClasses(y):
+    return np.argmax(y, axis=1)
+
+def CategoricalAccuracy(y_true, y_pred):
+    return np.mean(np.equal(PredictClasses(y_true), PredictClasses(y_pred)).astype(np.float32))
+
+def Matthews(y_true, y_pred):
+    return metrics.matthews_corrcoef(PredictClasses(y_true), PredictClasses(y_pred))
+
+
+_PROBA_LOSS_FN_MAP = {'log_loss': metrics.log_loss,
+                      'binary_crossentropy': metrics.log_loss,
+                      'categorical_crossentropy': metrics.log_loss,
+                      'categorical_accuracy': CategoricalAccuracy,
+                      'matthews': Matthews}
 
 class UnrecognisedLossException(Exception):
     pass
@@ -196,20 +218,23 @@ class UnrecognisedLossException(Exception):
 
 def DefaultPredictFn(model_, X, model_type=SKLEARN, classification=False,
                      proba=False, **kwargs):
+    y = None
     if model_type == SKLEARN:
         if proba:
-            return model_.predict_proba(X)
+            y = model_.predict_proba(X)
         else:
-            return model_.predict(X)
+            y = model_.predict(X)
     elif model_type == KERAS:
         if classification:
             if proba:
-                return model_.predict_proba(X)[:, 0]
+                y = model_.predict_proba(X)
             else:
-                return model_.predict_classes(X)[:, 0]
+                y = model_.predict_classes(X)
         else:
-            return model_.predict(X)[:, 0]
-
+            y = model_.predict(X)
+    if y.ndim == 2 and y.shape[1] == 1:
+        return np.squeeze(y, axis=1) 
+    return y
 
 def DefaultEvalFn(model_, X, y, loss='mse', metrics=[], **kwargs):
     #print('Loss: %s' % loss)
@@ -218,7 +243,7 @@ def DefaultEvalFn(model_, X, y, loss='mse', metrics=[], **kwargs):
     if isinstance(y, pd.DataFrame) or isinstance(y, pd.Series):
         y = y.as_matrix()
     
-    classification = (loss == 'log_loss')
+    classification = (loss in _PROBA_LOSS_FN_MAP)
     to_find = set([loss] + metrics)
     normal_loss_fns = []
     proba_loss_fns = []
@@ -235,8 +260,6 @@ def DefaultEvalFn(model_, X, y, loss='mse', metrics=[], **kwargs):
                             classification=classification,
                             proba=False,
                             **kwargs)
-    #print(y)
-    #print(pred)
     for loss_type in normal_loss_fns:
         losses[loss_type] = _LOSS_FN_MAP[loss_type](y, pred)
 
@@ -314,6 +337,9 @@ def CrossEvaluate(X, y, model_fn, **kwargs):
 
         model_, loss = train_fn(model_fn, eval_fn, train_X, train_y, **kwargs)
         
+        if model_ is None:
+            return None, None
+        
         results[fold_name] = eval_fn(model_, test_X, test_y, **kwargs)
         performance[fold_name] = results[fold_name].loss
         if verbose:
@@ -327,11 +353,14 @@ def CrossEvaluate(X, y, model_fn, **kwargs):
     return final_performance, results
 
 
-def MakeNN(hidden_layers, loss='mse', reg_strength=0.0):
-   
+def MakeNN(hidden_layers, n_outputs=1, loss='mse', reg_strength=0.0, init_stddev=1.0, optimizer='adam'):
+  
     if loss == 'log_loss':
         loss = 'binary_crossentropy'
+    if loss  == 'binary_crossentropy':
         output_activation = 'sigmoid'
+    elif loss == 'categorical_crossentropy':
+        output_activation = 'softmax'
     else:
         output_activation = 'relu'
 
@@ -339,23 +368,32 @@ def MakeNN(hidden_layers, loss='mse', reg_strength=0.0):
         nn = models.Sequential()
 
         nn.add(layers.Dense(units=hidden_layers[0], input_dim=X.shape[1],
-                            kernel_initializer='normal',
+                            kernel_initializer=initializers.RandomNormal(mean=0.0,
+                                                                         stddev=init_stddev),
+                            bias_initializer=initializers.RandomNormal(mean=0.0,
+                                                                       stddev=init_stddev),
                             bias_regularizer=regularizers.l2(reg_strength),
                             kernel_regularizer=regularizers.l2(reg_strength),
                             activation='relu'))
         for i in range(1, len(hidden_layers)):
             nn.add(layers.Dense(units=hidden_layers[i],
-                                kernel_initializer='normal',
+                                kernel_initializer=initializers.RandomNormal(mean=0.0,
+                                                                             stddev=init_stddev),
+                                bias_initializer=initializers.RandomNormal(mean=0.0,
+                                                                           stddev=init_stddev),
                                 bias_regularizer=regularizers.l2(reg_strength),
                                 kernel_regularizer=regularizers.l2(reg_strength),
                                 activation='relu'))
-        nn.add(layers.Dense(units=1,
-                            kernel_initializer='normal',
+        nn.add(layers.Dense(units=n_outputs,
+                            kernel_initializer=initializers.RandomNormal(mean=0.0,
+                                                                         stddev=init_stddev),
+                            bias_initializer=initializers.RandomNormal(mean=0.0,
+                                                                       stddev=init_stddev),
                             bias_regularizer=regularizers.l2(reg_strength),
                             kernel_regularizer=regularizers.l2(reg_strength),
                             activation=output_activation))
 
-        nn.compile(loss=loss, optimizer='adam')
+        nn.compile(loss=loss, optimizer=optimizer)
 
         return nn
     
